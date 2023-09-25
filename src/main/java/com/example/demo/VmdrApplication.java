40b9fe44e4d496f7484912ab314b7e869569e4e5
unknown <vaibhavkr127@gmail.com>
2023-09-25 17:30:23 +0530

    first commit

---
+++ src/main/java/com/example/demo/VmdrApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmdrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmdrApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/entity/Assets.java
@@ -0,0 +1,12 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="assets")
+public class Assets {
+	
+	
+
+}
